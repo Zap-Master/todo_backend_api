0653508a2fb1c58f086a476d8d47c0eb0e888873
Zap-Master <kim.lidiya2011@gmail.com>
2021-04-09 19:14:41 +0100

    added POST endpoint for creating new task

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const mysql = require('mysql');
 const pool = require('./database.js');
+const { request, response } = require('express');
 
 const app = express();
 app.use(cors());
@@ -11,18 +12,48 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.get("/tasks/:userId", function (request, response) {
+app.get('/tasks/:userId', function (request, response) {
     const userId = request.params.userId;
-    const sql = "SELECT * FROM todo.Tasks t WHERE t.userId = ?";
+    const sql = 'SELECT * FROM todo.Tasks t WHERE t.userId = ?';
     pool.query(sql, [userId],(err, data) => {
         if (err) {
-            console.log("Error from MySQL", err);
+            console.log('Error from MySQL', err);
             response.status(500).send(err);
         } else {
             response.status(200).send(data);
         }
     });
   });
+
+//create new task
+app.post('/createTask', (request, response) => {
+    const data = request.body;
+    const sql = 'insert into todo.Tasks(UserId, TaskName, DueDate, Complete, Description) values (?, ?, ?, ?, ?)';
+    pool.query(
+        sql,
+        [data.userId, data.taskName, data.dueDate, data.complete, data.description],
+        (err, results) => {
+            if (err) {
+                console.log('Error from MySQL', err);
+                response.status(500).send(err);
+            } else {
+                //send back new task
+                pool.query(
+                    'SELECT * FROM todo.Tasks t WHERE t.taskId = ?',
+                    [results.insertId],
+                    (err, results) => {
+                        if (err) {
+                            console.log('Error from MySQL', err);
+                            response.status(500).send(err);
+                        } else {
+                            response.status(201).send(results[0]);
+                        }
+                    }
+                )
+            }
+        }
+    )
+})
 
 
 module.exports.handler = serverless(app);
