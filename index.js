204170b2e7d30e3c0814f2fdd99ea3bb16a98743
Zap-Master <kim.lidiya2011@gmail.com>
2021-04-09 15:35:50 +0100

    implement GET endpoint for all tasks

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const serverless = require('serverless-http');
+const cors = require('cors');
 const express = require('express');
-const app = express();
+const mysql = require('mysql');
 
-app.get('/', function(req, res) {
-    res.json({'message': 'Hello World!'})
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get("/tasks", function (request, response) {
+    connection.query("SELECT * FROM Tasks", function (err, data) {
+      if (err) {
+        console.log("Error from MySQL", err);
+        response.status(500).send(err);
+      } else {
+        response.status(200).send(data);
+      }
+    });
+  });
+
 
 module.exports.handler = serverless(app);
